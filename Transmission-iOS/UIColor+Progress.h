b3e0987743bbdd323d39e2d79a724618b269d237
Brian Partridge <brianpartridge@gmail.com>
2013-03-03 20:11:31 -0500

    pull in images and colors from main Transmission project

---
+++ Transmission-iOS/UIColor+Progress.h
@@ -0,0 +1,24 @@
+//
+//  UIColor+Progress.h
+//  Transmission-iOS
+//
+//  Created by Brian Partridge on 3/3/13.
+//  Copyright (c) 2013 Brian Partridge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Progress)
+
++ (UIColor *) progressBlueColor;
++ (UIColor *) progressDarkBlueColor;
++ (UIColor *) progressDarkGreenColor;
++ (UIColor *) progressGrayColor;
++ (UIColor *) progressGreenColor;
++ (UIColor *) progressLightGrayColor;
++ (UIColor *) progressLightGreenColor;
++ (UIColor *) progressRedColor;
++ (UIColor *) progressWhiteColor;
++ (UIColor *) progressYellowColor;
+
+@end
